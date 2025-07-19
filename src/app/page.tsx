7fa408cdf38b18f2c0c5df81317c07f16a1d9fe8
lrmn7 <lrmn.dev@gmail.com>
2025-07-19 22:49:59 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useReadContract } from "wagmi";
 import { contractConfig } from "@/lib/constants";
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import somfeedLogo from "/somfeed.svg";
+import somfeedLogo from "../../public/somfeed.svg";
 type Post = {
   id: bigint;
   author: `0x${string}`;
